60bd965b563504135f565c1bf179ff711872563c
ludovicmantovani <ludovic.mantov@gmail.com>
2023-03-13 13:50:44 +0100

    [UPDATE] : Use key to move light

--- Assets/Script/Player/MoveSpotLight.cs
+++ Assets/Script/Player/MoveSpotLight.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class MoveSpotLight : MonoBehaviour
 {
     public float speed = 10f;
-    public float range = 4f;
+    public float rangeX = 10f;
+    public float rangeY = 4f;
 
     [SerializeField] float horizontal = 0f;
     [SerializeField] float vertical = 0f;
+    [SerializeField] bool canMove = true;
 
-    // Update is called once per frame
+    [SerializeField] float spotLightZpos = 10f;
+
+    private void Start()
+    {
+        transform.position.Set(
+            0,
+            0,
+            spotLightZpos);
+    }
+
     void Update()
     {
-        horizontal = Input.GetAxis("Mouse X");
-        vertical = Input.GetAxis("Mouse Y");
+        if (canMove)
+            Locomotion();
+    }
+
+
+    private void Locomotion()
+    {
+        //horizontal = Input.GetAxis("Mouse X");
+        horizontal = Input.GetAxis("Horizontal");
+        //vertical = Input.GetAxis("Mouse Y");
+        vertical = Input.GetAxis("Vertical");
 
         // Déplacement horizontal de la Spot Light
         transform.position += new Vector3(horizontal * speed * Time.deltaTime, 0f, 0f) * -1;
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -range, range), transform.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -rangeX, rangeX), transform.position.y, transform.position.z);
 
         // Déplacement vertical de la Spot Light
         transform.position += new Vector3(0f, vertical * speed * Time.deltaTime, 0f);
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -range, range), transform.position.z);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -rangeY, rangeY), transform.position.z);
     }
 }
